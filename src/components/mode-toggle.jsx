4cd49b070ddc583e52fb3dd05d571ca8a752bf11
ahmed mbarek <100807174+ahmedmbarektorjmen@users.noreply.github.com>
2024-02-18 09:54:14 +0000

    toggle working

--- src/components/mode-toggle.jsx
+++ src/components/mode-toggle.jsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -16,15 +16,29 @@ import {
 export function ModeToggle() {
   const { systemTheme , theme, setTheme } = useTheme()
   const [currentTheme, setCurrentTheme] = useState(theme === "system" ? systemTheme:theme);
+  const [ mounted, setMounted ] = useState(false)
+
+  useEffect(()=>{
+    setMounted(true);
+  },[])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
-        <Button variant="outline" size="icon" onClick={()=> (currentTheme === "dark") ? {
-          setTheme("light");
-          setCurrentTheme("light");
-          }:{
+        <Button variant="outline" size="icon" onClick={()=> {
+          if (currentTheme === "dark") {
+            setTheme("light");
+            setCurrentTheme("light");
+        }
+          else {
             setTheme("dark");
             setCurrentTheme("dark");
-            }}>
+          }
+        }
+      }
+          >
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
